8e57b2a8b566f5210304cc1539719dc772da7c9f
Linad-Nikoros <linad.nikoros@gmail.com>
2022-12-11 19:20:04 +0700

    solved the control task

---
+++ 1_task/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+
+
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array1=new string[size];
+    string[] array2=new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array1[i] = Console.ReadLine();
+    }
+    
+SelectLengThree(array1,array2);
+PrintArray(array2);
+
+void SelectLengThree(string[] array0, string[] array1)
+{
+    
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array0[i].Length <= 3)
+        {
+            array1[i] = array0[i];
+        }
+
+    }
+ 
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (!string.IsNullOrEmpty(array[i]))
+        {
+            Console.WriteLine($"{array[i]}");
+        }
+    }
+}
+
